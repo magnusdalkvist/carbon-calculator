2132199f7c506bd33f5a0a6e7e3053e9a851a754
Lasse Godtkjaer <lass417r@stud.kea.dk>
2022-11-18 11:33:40 +0100

    Lasse commit

---
+++ src/components/c02data.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function c02data() {
+  const facebookVal = 0.79;
+  const youtubeVal = 0.46;
+  const twitchVal = 0.55;
+  const twitterVal = 0.6;
+  const linkedinVal = 0.71;
+  const snapchatVal = 0.87;
+  const instagramVal = 1.05;
+  const pinterestVal = 1.3;
+  const redditVal = 2.48;
+  const tiktokVal = 2.63;
+
+  return <div>c02data</div>;
+}
+
+export default function c02Calc() {
+
+    return <div></div>;
+  }
